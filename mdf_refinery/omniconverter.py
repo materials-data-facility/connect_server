f9b5ef1fd86cc354b53c6fd9c5f0c466ff1f032b
jgaff <jgaff@uchicago.edu>
2017-10-25 14:27:39 -0500

    Checkpoint in API /convert work
    Not functional at this time, just backing up changes.

--- mdf_refinery/omniconverter.py
+++ mdf_refinery/omniconverter.py
@@ -5,12 +5,12 @@ from copy import deepcopy
 from tqdm import tqdm
 
 from mdf_forge import toolbox
-from mdf_refinery.validator import Validator
+from mdf_refinery import validator
 
 from omniparser import omniparse
 
 
-def omniconvert(input_path, all_metadata, verbose=False):
+def omniconvert(input_path, all_metadata, feedstock_path, verbose=False):
     """Convert a dataset into MDF feedstock as best as possible.
 
     Arguments:
@@ -18,10 +18,13 @@ def omniconvert(input_path, all_metadata, verbose=False):
     all_metadata (dict): The metadata for the dataset and record entries.
         This dict should be composed of the keys "dataset" and "record", for each
         metadata type.
+    feedstock_path (str): File to store feedstock.
+                          If None, will return feedstock instead of writing out.
     verbose (bool): If True, will print status messages. Default False.
 
     Returns:
-    None
+    None (if feedstock_path is not None)
+    dict (if feedstock_path is None): The feedstock.
 
     Outputs:
     Uses the Validator to output feedstock into the appropriate directory.
@@ -29,26 +32,58 @@ def omniconvert(input_path, all_metadata, verbose=False):
     if verbose:
         print("Begin converting")
 
-    dataset_validator = Validator(all_metadata["dataset"])
-    source_name = all_metadata["dataset"]["mdf"]["source_name"]
-    all_metadata["record"][source_name] = all_metadata["record"].get(source_name, {})
+    ds_md = {
+        "dc": all_metadata.get("dc"),
+        "mdf": all_metadata.get("mdf")
+        }
+    rc_md_template = {
+        "mdf": all_metadata.get("mdf"),
+        "files": [],
+        "materials": {}
+        }
+    source_name = all_metadata.get("mdf", {}).get("source_name")
 
-    success_count = 0
-    failures = []
-    for file_data in tqdm(toolbox.find_files(input_path), desc="Processing files", disable= not verbose):
-        path = os.path.join(file_data["path"], file_data["filename"])
-        record_metadata = deepcopy(all_metadata["record"])
-        res, info = omniparse(path, info=True)
-        if res:
-            record_metadata[source_name].update(res)
-            val = dataset_validator.write_record(record_metadata)
-            if not val["success"]:
-                if not dataset_validator.cancel_validation()["success"]:
-                    print("Error cancelling validation. The partial feedstock may not be removed.")
-                raise ValueError(val["message"] + "\n" + val.get("details", ""))
-            success_count += 1
+    all_feedstock = None
+    with open(feedstock_path or os.devnull, 'w') as feedstock_file:
+        # Handle dataset entry
+        dataset_result = validator.validate_dataset(ds_md)
+        if not dataset_result["success"]:
+            raise ValueError(dataset_result["error"])
+        dataset_metadata = dataset_result["valid"]
+        if feedstock_path:
+            json.dump(dataset_metadata, feedstock_file)
+            feedstock_file.write("\n")
         else:
-            failures.append(path)
+            all_feedstock = [dataset_metadata]
+
+        # Index records
+        success_count = 0
+        failures = []
+        for file_data in tqdm(toolbox.find_files(input_path), desc="Processing files", disable= not verbose):
+            path = os.path.join(file_data["path"], file_data["filename"])
+            rc_md = deepcopy(rc_md_template)
+            res, info = omniparse(path, info=True)
+            if res:
+                rc_md[info["parser"]] = res
+                record_result = validator.validate_record(rc_md)
+                if not record_result["success"]:
+                    raise ValueError(record_result["error"])
+                record_metadata = record_result["valid"]
+                if feedstock_path:
+                    json.dump(record_metadata, feedstock_file)
+                    feedstock_file.write("\n")
+                else:
+                    all_feedstock.append(record_metadata)
+
+
+                val = dataset_validator.write_record(record_metadata)
+                if not val["success"]:
+                    if not dataset_validator.cancel_validation()["success"]:
+                        print("Error cancelling validation. The partial feedstock may not be removed.")
+                    raise ValueError(val["message"] + "\n" + val.get("details", ""))
+                success_count += 1
+            else:
+                failures.append(path)
 
     if verbose:
         print("Finished converting\nProcessed:", success_count, "\Failed:", len(failures))
--- services/api_convert.py
+++ services/api_convert.py
@@ -4,10 +4,12 @@ import json
 import zipfile
 from threading import Thread
 
+import requests
 from bson import ObjectId
 from flask import Flask, request
 
 from mdf_forge import toolbox
+from mdf_refinery.omniconverter import omniconvert
 
 app = Flask(__name__)
 app.config.from_pyfile("api.conf")
@@ -44,24 +46,26 @@ def begin_convert(metadata):
         }
     mdf_transfer_client = toolbox.confidential_login(creds)["transfer"]
 
-    success = False
+    local_success = False
     backup_tid = None
     user_tid = None
     local_path = os.path.join(app.config["LOCAL_PATH"], metadata["mdf_status_id"]) + "/"
     os.mkdir(local_path)
 
+    # Download data locally
     if metadata.get("zip"):
+        # Download and unzip
         zip_path = os.path.join(local_path, metadata["mdf_status_id"] + ".zip")
         res = requests.get(metadata["zip"])
         with open(zip_path, 'wb') as out:
             out.write(res.content)
         zipfile.ZipFile(zip_path).extractall(local_path)
-        os.remove(zip_path)
-        # Do stuff with data
+        os.remove(zip_path) #TODO: Should the .zip be removed?
+        local_success = True
 
-        backup_tid = toolbox.quick_transfer(mdf_transfer_client, app.config["LOCAL_EP"], app.config["BACKUP_EP"], [(local_path, app.config["BACKUP_PATH"])], timeout=-1)
-        success = True
     elif metadata.get("globus"):
+        #TODO: test globus
+        raise NotImplementedError("Prototyping .zip download only")
         user_ep, user_path = metadata["globus"].split("/", 1)
         user_path = "/" + user_path
         if user_path.endswith("/"):
@@ -69,13 +73,38 @@ def begin_convert(metadata):
         else:
             local_path = os.path.join(local_path, metadata["mdf_status_id"] + ".file")
             remote_path = os.path.join(app.config["BACKUP_PATH"], metadata["mdf_status_id"] + ".file")
-        user_tid = toolbox.quick_transfer(mdf_transfer_client, user_ep, app.config["LOCAL_EP"], [(user_path, local_path)])
-        # Do stuff with data
-        backup_tid = toolbox.quick_transfer(mdf_transfer_client, app.config["LOCAL_EP"], app.config["BACKUP_EP"], [(local_path, remote_path)], timeout=-1)
-        success = True
+        user_tid = toolbox.quick_transfer(mdf_transfer_client, user_ep, app.config["LOCAL_EP"], [(user_path, local_path)], timeout=0)
+        local_success = True
+
+    elif metadata.get("files"):
+        # TODO: Implement this
+        pass
+
     else:
         # Nothing to do
         pass
+
+    #TODO: Update status - download success/failure
+    if not local_success:
+        raise IOError("No data downloaded")
+
+    # Backup data
+    backup_tid = toolbox.quick_transfer(mdf_transfer_client, app.config["LOCAL_EP"], app.config["BACKUP_EP"], [(local_path, app.config["BACKUP_PATH"])], timeout=0)
+    #TODO: Update status - backup success
+
+    # Trigger omniconverter
+    try:
+        feedstock = omniconvert(local_path, metadata, feedstock_path=None)
+    except Exception as e:
+        #TODO: Update status - indexing failed
+        raise
+    #TODO: Update status - indexing success
+
+    # Pass feedstock to /ingest
+    requests.post(app.config["INGEST_URL"], data=)
+
+
+
 
     # Remove local data
     # TODO: Remove data after transfer success
