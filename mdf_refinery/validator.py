9d6fef28653acb2f75e52665d9bed353760d9a06
jgaff <jgaff@uchicago.edu>
2017-10-26 16:03:27 -0500

    Validator improvements, /ingest continuation
    Add 'finalize' flag to change validator behavior into finalizer. Improve schema reading in validator to simplify schema writing. Continue initial development of /ingest route.

--- mdf_refinery/validator.py
+++ mdf_refinery/validator.py
@@ -3,18 +3,41 @@ import json
 
 import jsonschema
 
-DATASET_SCHEMA = os.path.join(os.path.dirname(__file__), "dataset.schema")
-RECORD_SCHEMA = os.path.join(os.path.dirname(__file__), "record.schema")
+SCHEMA_DIR = os.path.join(os.path.dirname(__file__), "schemas")
 
-    
-def validate_dataset(ds_md):
-    """Validate a dataset"""
+
+def validate_dataset(ds_md, finalize=False):
+    """Validate a dataset against the MDF schema.
+
+    Arguments:
+    ds_md (dict): The dataset metadata to validate.
+    finalize (bool): Is this the finalizing validation before ingestion?
+                     For all purposes except some MDF internal services, thish should be False.
+
+    Returns:
+    dict: success (bool): True on success, False on failure
+        If success is True:
+          valid (dict): The validated and processed metadata.
+        If success is False:
+          error (str): A short message about the error.
+          details (str): The full jsonschema error message.
+    """
+    # Load schema
+    with open(os.path.join(SCHEMA_DIR, "basic_dataset.schema")) as base_schema_file:
+        schema = json.load(base_schema_file)
+    # If finalizing, load final additions to schema
+    if finalize:
+        with open(os.path.join(SCHEMA_DIR, "final_dataset.schema")) as final_schema_file:
+            schema = deep_merge(schema, json.load(final_schema_file)
+
     # Add validator fields
     ds_md["metadata_version"] = "0.5.0"
 
+    # Add finalizer fields
+    if finalize:
+        ds_md["finalized"] = True
+
     # Validate against schema
-    with open(DATASET_SCHEMA) as schema_file:
-        schema = json.load(schema_file)
     try:
         jsonschema.validate(ds_md, schema)
     except jsonschema.ValidationError as e:
@@ -30,14 +53,38 @@ def validate_dataset(ds_md):
         }
 
 
-def validate_record(rc_md):
-    """Validate a record."""
+def validate_record(rc_md, finalize=False):
+    """Validate a record against the MDF schema.
+
+    Arguments:
+    rc_md (dict): The record metadata to validate.
+    finalize (bool): Is this the finalizing validation before ingestion?
+                     For all purposes except some MDF internal services, this should be False.
+
+    Returns:
+    dict: success (bool): True on success, False on failure
+        If success is True:
+          valid (dict): The validated and processed metadata.
+        If success is False:
+          error (str): A short message about the error.
+          details (str): The full jsonschema error message.
+    """
+    # Load schema
+    with open(os.path.join(SCHEMA_DIR, "basic_record.schema")) as base_schema_file:
+        schema = json.load(base_schema_file)
+    # If finalizing, load final additions to schema
+    if finalize:
+        with open(os.path.join(SCHEMA_DIR, "final_record.schema")) as final_schema_file:
+            schema = deep_merge(schema, json.load(final_schema_file)
+
     # Add validator fields
     rc_md["metadata_version"] = "0.5.0"
 
+    # Add finalizer fields
+    if finalize:
+        rc_md["finalized"] = True
+
     # Validate against schema
-    with open(RECORD_SCHEMA) as schema_file:
-        schema = json.load(schema_file)
     try:
         jsonschema.validate(rc_md, schema)
     except jsonschema.ValidationError as e:
@@ -50,5 +97,25 @@ def validate_record(rc_md):
     return {
         "success": True,
         "valid": rc_md
-        }    
+        }
+
+
+# Utility function
+# Will merge into Toolbox if useful elsewhere
+def deep_merge(base, update):
+    """Deeply update a base dictionary with values from an update dictionary.
+
+    Arguments:
+    base (dict): The default dictionary.
+    update (dict): The overwriting dictionary.
+
+    Returns:
+    dict: The base dictionary with the values in the update dictionary added.
+    """
+    for key, value in update.items():
+        if isinstance(value, collections.Mapping):
+            base[key] = update(base.get(key, {}), value)
+        else:
+            base[key] = value
+    return base
 
--- services/api_ingest.py
+++ services/api_ingest.py
@@ -13,7 +13,12 @@ app.config.from_pyfile("api.conf")
 
 @app.route("/ingest", methods=["POST"])
 def accept_ingest():
-    """Accept the JSON feedstock and begin the ingestion process."""
+    """Accept the JSON feedstock file and begin the ingestion process."""
+    if not request.files.get("file"):
+        return {
+            "success": False,
+            "error": "No feedstock file uploaded"
+            }
     feedstock = request.get_json(force=True, silent=True)
     if not feedstock:
         return {
@@ -29,9 +34,10 @@ def accept_ingest():
         dataset["mdf_status_id"] = status_id
         #TODO: Register status ID
     else:
+        #TODO: Check that status exists (must not be set by user)
         #TODO: Update status - ingest request recieved
         status_id = dataset["mdf_status_id"]
-    ingester = Thread(target=begin_ingest, name="ingester_thread", args=(dataset,records))
+    ingester = Thread(target=begin_ingest, name="ingester_thread", args=(dataset, records))
     ingester.start()
     return {
         "success": True,
@@ -50,8 +56,21 @@ def begin_ingest(dataset, records):
         }
     search_client = toolbox.confidential_login(creds)["search_client"]
 
-    # Validate feedstock
-    dataset_result = validator.validate_dataset(dataset)
-    if 
+    # Finalize feedstock
+    final_feedstock = []
+    dataset_result = validator.validate_dataset(dataset, finalize=True)
+    if not dataset_result["success"]:
+        # TODO: Update status - dataset validation failed
+        return dataset_result
+    final_feedstock.append(dataset_result["valid"])
+    for rc in records:
+        rc_result = validator.validate_record(rc, finalize=True)
+        if not rc_result["success"]:
+            #TODO: Update status - record validation failed
+            return rc_result
+        final_feedstock.append(rc_result["valid"])
+    #TODO: Update status - validation passed
+
+    #TODO: Ingest finalized feedstock
 
 
