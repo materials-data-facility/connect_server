b99e24e32a352bb673c0448261fbb32c6097ad60
jgaff <jgaff@uchicago.edu>
2018-02-28 09:10:21 -0600

    EM parser bugfixes

--- mdf_refinery/transformer.py
+++ mdf_refinery/transformer.py
@@ -384,37 +384,42 @@ def parse_electron_microscopy(group, params=None):
         try:
             data = hs.load(file_path).metadata.as_dictionary()
         except Exception as e:
+            print(repr(e))
             pass
         else:
+            print(data)
             em = {}
             # Image mode is SEM, TEM, or STEM.
             # STEM is a subset of TEM.
             if "SEM" in data.get('Acquisition_instrument', {}).keys():
                 inst = "SEM"
-            if "TEM" in data.get('Acquisition_instrument', {}).keys():
+            elif "TEM" in data.get('Acquisition_instrument', {}).keys():
                 inst = "TEM"
+            else:
+                inst = "None"
             em['beam_current'] = (data.get('Acquisition_instrument', {}).get(inst, {})
-                                      .get('beam_current'))
+                                      .get('beam_current', None))
             em['beam_energy'] = (data.get('Acquisition_instrument', {}).get(inst, {})
-                                     .get('beam_energy'))
+                                     .get('beam_energy', None))
             em['magnification'] = (data.get('Acquisition_instrument', {}).get(inst, {})
-                                       .get('magnification'))
+                                       .get('magnification', None))
             em['microscope'] = (data.get('Acquisition_instrument', {}).get(inst, {})
-                                    .get('microscope'))
+                                    .get('microscope', None))
             em['image_mode'] = (data.get('Acquisition_instrument', {}).get(inst, {})
-                                    .get('acquisition_mode'))
+                                    .get('acquisition_mode', None))
             detector = (data.get('Acquisition_instrument', {}).get(inst, {})
-                            .get('Detector'))
+                            .get('Detector', None))
             if detector:
                 em['detector'] = next(iter(detector))
 
             # Remove None values
             for key, val in list(em.items()):
-                if not val:
+                if val is None:
                     em.pop(key)
-            records.append({
-                "electron_microscopy": em
-            })
+            if em:
+                records.append({
+                    "electron_microscopy": em
+                })
     return records
 
 
