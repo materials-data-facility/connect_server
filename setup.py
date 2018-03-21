9f7db2b337a8e3b50a4552a5bd43ba1f150b8919
jgaff <jgaff@uchicago.edu>
2018-03-21 20:19:59 +0000

    Update requirements

--- setup.py
+++ setup.py
@@ -8,21 +8,24 @@ setup(
     description='Materials Data Facility Connect infrastructure',
     install_requires=[
         "ase>=3.15.0",
-        "boto3>=1.5.22",
-        "citrination-client>=3.1.0",
+        "boto3>=1.5.30",
+        "citrination-client>=3.2.0",
         "crossrefapi>=1.2.0",
+        "dfttopif>=1.0.0",
         "flask>=0.12.2",
-        "globus-sdk>=1.4.1",
+        "globus-sdk>=1.5.0",
+        "hyperspy>=1.3",
         "jsonschema>=2.6.0",
         "mdf-toolbox>=0.1.7",
-        "pandas>=0.20.3",
+        "pandas>=0.22.0",
         "pif-ingestor",
-        "Pillow>=3.1.2",
-        "pymatgen>=2017.10.16",
+        "Pillow>=5.0.0",
+        "pymatgen>=2018.2.13",
         "pymongo>=3.5.1",  # For bson.ObjectId
         "pypif>=2.1.0",
         "pypif-sdk",
-        "python-magic>=0.4.13",
+        "python-magic>=0.4.15",
+        "pyyaml>=3.12",
         "requests>=2.18.4"
     ]
 )
