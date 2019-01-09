31822bd3d36ebcf95d1a73ce5d3dfae84d519667
jgaff <jgaff@uchicago.edu>
2019-01-09 15:13:29 -0600

    Add new grouping functionality - read mdf.json config and obey flags

--- mdf_connect_server/config/conversion.py
+++ mdf_connect_server/config/conversion.py
@@ -1,67 +1,69 @@
 CONVERSION = {
     "GROUPING_RULES": {
-        "cif": {
-            "files": [
-                ".cif",
-            ],
-            "parsers": [
-                "crystal_structure",
-                "pif"
-            ],
-            "params": {
-                "include": [
-                    "cif",
-                    "dft"
-                ]
-            }
-        },
-        "vasp": {
-            "files": [
-                "outcar",
-                "incar",
-                "chgcar",
-                "wavecar",
-                "wavcar",
-                "ozicar",
-                "ibzcar",
-                "kpoints",
-                "doscar",
-                "poscar",
-                "contcar",
-                "vasp_run.xml",
-                "xdatcar"
-            ],
-            "parsers": [
-                "pif",
-                "crystal_structure"
-            ],
-            "params": {
-                "include": [
-                    "dft"
-                ]
-            }
-        },
-        "json": {
-            "files": [
-                ".json"
-            ],
-            "parsers": [
-                "json"
-            ],
-            "params": {}
-        },
-        "csv": {
-            "files": [
-                ".csv"
-            ],
-            "parsers": [
-                "csv",
-                "pif"
-            ],
-            "params": {
-                "include": [
-                    "template_csv"
-                ]
+        "known_formats": {
+            "cif": {
+                "files": [
+                    ".cif",
+                ],
+                "parsers": [
+                    "crystal_structure",
+                    "pif"
+                ],
+                "params": {
+                    "include": [
+                        "cif",
+                        "dft"
+                    ]
+                }
+            },
+            "vasp": {
+                "files": [
+                    "outcar",
+                    "incar",
+                    "chgcar",
+                    "wavecar",
+                    "wavcar",
+                    "ozicar",
+                    "ibzcar",
+                    "kpoints",
+                    "doscar",
+                    "poscar",
+                    "contcar",
+                    "vasp_run.xml",
+                    "xdatcar"
+                ],
+                "parsers": [
+                    "pif",
+                    "crystal_structure"
+                ],
+                "params": {
+                    "include": [
+                        "dft"
+                    ]
+                }
+            },
+            "json": {
+                "files": [
+                    ".json"
+                ],
+                "parsers": [
+                    "json"
+                ],
+                "params": {}
+            },
+            "csv": {
+                "files": [
+                    ".csv"
+                ],
+                "parsers": [
+                    "csv",
+                    "pif"
+                ],
+                "params": {
+                    "include": [
+                        "template_csv"
+                    ]
+                }
             }
         }
     },
--- mdf_connect_server/processor/converter.py
+++ mdf_connect_server/processor/converter.py
@@ -5,6 +5,8 @@ import multiprocessing
 import os
 from queue import Empty
 
+import mdf_toolbox
+
 from mdf_connect_server import CONFIG
 from mdf_connect_server.processor import transform
 
@@ -43,7 +45,7 @@ def convert(root_path, convert_params):
     # Populate input queue
     num_groups = 0
     extensions = set()
-    for group_info in group_tree(root_path):
+    for group_info in group_tree(root_path, CONFIG["GROUPING_RULES"]):
         input_queue.put(group_info)
         num_groups += 1
         for f in group_info["files"]:
@@ -94,6 +96,77 @@ def convert(root_path, convert_params):
     return (feedstock, num_groups, list(extensions))
 
 
+def group_tree(root, config):
+    """Run group_files on files in tree appropriately."""
+    files = []
+    dirs = []
+    for node in os.listdir(root):
+        node_path = os.path.join(root, node)
+        if node == "mdf.json":
+            with open(node_path) as f:
+                try:
+                    new_config = json.load(f)
+                except Exception as e:
+                    logger.warning("Error reading config file '{}': {}".format(node_path, str(e)))
+                else:
+                    config = mdf_toolbox.dict_merge(new_config, config)
+        elif os.path.isfile(node_path):
+            files.append(node_path)
+        elif os.path.isdir(node_path):
+            dirs.append(node_path)
+        else:
+            logger.debug("Ignoring non-file, non-dir node '{}'".format(node_path))
+
+#    grouping = group_files(files, config)
+    # Group the files
+    # list "groups" is list of dict, each dict contains actual file list + parser info/config
+    groups = []
+    # Group by dir overrides other grouping
+    if config.get("group_by_dir"):
+        groups.append({"files": files,
+                       "parsers": [],
+                       "params": config})
+    else:
+        for format_rules in config.get("known_formats", {}).values():
+            format_name_list = format_rules["files"]
+            format_groups = {}
+            # Check each file for rule matching
+            # Match to appropriate group (with same pre/post pattern)
+            #   eg a_[match]_b groups with a_[other match]_b but not c_[other match]_d
+            for f in files:
+                fname = os.path.basename(f).lower().strip()
+                for format_name in format_name_list:
+                    if format_name in fname:
+                        pre_post_pattern = fname.replace(format_name, "")
+                        if not format_groups.get(pre_post_pattern):
+                            format_groups[pre_post_pattern] = []
+                        format_groups[pre_post_pattern].append(f)
+                        break
+            # Remove grouped files from the file list and add groups to the group list
+            for g in format_groups.values():
+                for f in g:
+                    files.remove(f)
+                group_info = {
+                    "files": [os.path.join(path, f) for f in g],
+                    "parsers": format_rules["parsers"],
+                    "params": format_rules["params"]
+                }
+                groups.append(group_info)
+
+        # NOTE: Keep this grouping last!
+        # Default grouping: Each file is a group
+        groups.extend([{"files": [f],
+                        "parsers": [],
+                        "params": {}}
+                       for f in files])
+
+    [groups.extend(group_tree(d, config)) for d in dirs]
+
+    return groups
+
+
+######OLD
+'''
 def group_tree(root):
     """Group files based on format-specific rules."""
     for path, dirs, files in os.walk(os.path.abspath(root)):
@@ -137,6 +210,7 @@ def group_tree(root):
         # Yield each group
         for g in groups:
             yield g
+'''
 
 
 def expand_repository_tags(input_tags, repo_rules=CONFIG["REPOSITORY_RULES"]):
