4b2f67a42e7b0416d5bd3221da2d196f3c6de0d3
jgaff <jgaff@uchicago.edu>
2019-06-05 17:34:49 +0000

    Add MDF Connect Admins to ACL instead of just Connect

--- mdf_connect_server/api/api.py
+++ mdf_connect_server/api/api.py
@@ -216,9 +216,9 @@ def accept_submission():
     # If ACL includes "public", no other entries needed
     if "public" in sub_conf["acl"]:
         sub_conf["acl"] = ["public"]
-    # Otherwise, make sure Connect has permission, also deduplicate
+    # Otherwise, make sure Connect admins have permission, also deduplicate
     else:
-        sub_conf["acl"].append(CONFIG["API_CLIENT_ID"])
+        sub_conf["acl"].append(CONFIG["ADMIN_GROUP_ID"])
         sub_conf["acl"] = list(set(sub_conf["acl"]))
     # Set correct ACL in metadata
     metadata["mdf"]["acl"] = sub_conf["acl"]
