60a69771dc90cf9b2c412ba11dda94f271cec373
jgaff <jgaff@uchicago.edu>
2019-07-31 11:27:15 -0500

    Update EM and image parsers to new schema

--- mdf_connect_server/processor/transformer.py
+++ mdf_connect_server/processor/transformer.py
@@ -452,9 +452,10 @@ def parse_image(group, params=None):
             im = Image.open(file_path)
             records.append({
                 "image": {
-                    "width": im.width,
-                    "height": im.height,
-                    "megapixels": (im.width * im.height) / 1000000
+                    "shape": [
+                        im.width,
+                        im.height
+                    ]
                 }
             })
         except Exception:
@@ -467,11 +468,14 @@ def parse_electron_microscopy(group, params=None):
     records = []
     for file_path in group:
         try:
-            data = hs.load(file_path).metadata.as_dictionary()
+            hs_data = hs.load(file_path)
+            data = hs_data.metadata.as_dictionary()
+            raw_data = hs_data.original_metadata.as_dictionary()
         except Exception:
             pass
         else:
             em = {}
+            image = {}
             # Image mode is SEM, TEM, or STEM.
             # STEM is a subset of TEM.
             if "SEM" in data.get('Acquisition_instrument', {}).keys():
@@ -480,20 +484,81 @@ def parse_electron_microscopy(group, params=None):
                 inst = "TEM"
             else:
                 inst = "None"
-            em['beam_energy'] = (data.get('Acquisition_instrument', {}).get(inst, {})
-                                     .get('beam_energy', None))
-            em['magnification'] = (data.get('Acquisition_instrument', {}).get(inst, {})
-                                       .get('magnification', None))
-            em['acquisition_mode'] = (data.get('Acquisition_instrument', {}).get(inst, {})
-                                          .get('acquisition_mode', None))
-            detector = (data.get('Acquisition_instrument', {}).get(inst, {})
-                            .get('Detector', None))
-            if detector:
-                em['detector'] = next(iter(detector))
 
-            # Remove None values
+            # HS data
+            try:
+                inst_data = data['Acquisition_instrument'][inst]
+            except KeyError:
+                pass
+            else:
+                try:
+                    em['beam_energy'] = inst_data['beam_energy']
+                except KeyError:
+                    pass
+                try:
+                    em['magnification'] = inst_data['magnification']
+                except KeyError:
+                    pass
+                try:
+                    em['acquisition_mode'] = inst_data['acquisition_mode']
+                except KeyError:
+                    pass
+                try:
+                    detector = inst_data['Detector']
+                except KeyError:
+                    pass
+                else:
+                    em['detector'] = next(iter(detector))
+
+            # Non-HS data (not pulled into standard HS metadata)
+            # Pull out common dicts
+            try:
+                micro_info = raw_data["ImageList"]["TagGroup0"]["ImageTags"]["Microscope Info"]
+            except KeyError:
+                micro_info = {}
+            try:
+                exp_desc = raw_data["ObjectInfo"]["ExperimentalDescription"]
+            except KeyError:
+                exp_desc = {}
+
+            # emission_current
+            try:
+                em["emission_current"] = micro_info["Emission Current (µA)"]
+            except KeyError:
+                try:
+                    em["emission_current"] = exp_desc["Emission_uA"]
+                except KeyError:
+                    pass
+            # operation_mode
+            try:
+                em["operation_mode"] = micro_info["Operation Mode"]
+            except KeyError:
+                pass
+            # microscope
+            try:
+                em["microscope"] = (raw_data["ImageList"]["TagGroup0"]["ImageTags"]
+                                            ["Session Info"]["Microscope"])
+            except KeyError:
+                try:
+                    em["microscope"] = micro_info["Name"]
+                except KeyError:
+                    pass
+            # spot_size
+            try:
+                em["spot_size"] = exp_desc["Spot size"]
+            except KeyError:
+                pass
+
+            # Image metadata
+            try:
+                image["shape"] = list(raw_data["ImageList"]["TagGroup0"]
+                                              ["ImageData"]["Dimensions"].values())
+            except KeyError:
+                pass
+
+            # Remove None/empty values
             for key, val in list(em.items()):
-                if val is None:
+                if val is None or val == [] or val == {}:
                     em.pop(key)
             if em:
                 records.append({
