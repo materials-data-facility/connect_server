f0ab4504bed22931577626b71e52befd948f412b
jgaff <jgaff@uchicago.edu>
2019-11-25 15:21:45 -0600

    Update FuncX function with org and mapping

--- prototypes/integrations/funcx_mdfcc.py
+++ prototypes/integrations/funcx_mdfcc.py
@@ -1,23 +1,35 @@
-def mdf_submit():
-    """Submit dataset to MDF Connect."""
+def mdf_submit(local_json_path, globus_uri, test=True, with_doi=True, update=False):
+    """Submit dataset to MDF Connect.
+
+    Arguments:
+        local_json_path (str): The path to the local JSON metadata file.
+        globus_uri (str): The URI to the Globus Endpoint and path, in the form:
+                "globus://[endpoint id]/[path to data directory]"
+        test (bool): Is this a test submission (test submissions generate test DOIs
+                and populate the Search index "mdf-test" instead of "mdf")?
+                Default True.
+        with_doi (bool): Should a DOI be minted? (Includes test DOI.)
+                Default True.
+        update (bool): Has this submission been made before? If so, an update will be made
+                to the previous submission. Test submissions and non-test submissions
+                are separate.
+    """
     import json
     import os
     from mdf_connect_client import MDFConnectClient
 
-    # TODO: Fill in or generate
-    path_to_json = ("/home/jgaff/Downloads/A002_SMB_A_BR_SAMPLE_noExtension_att0_Lq0"
-                    "_001_0001-0256.json")  # Local path to metadata file
-    globus_uri = "globus://endpoint-id/path/to/data/dir"  # Globus-accessible uri to data
-    test_submission = True  # Is this a test submission?
-    publish_with_doi = True  # Should a DOI be minted? (Includes test DOI)
-    is_resubmission = False  # Has this submission been made before?
-
-    mapping = {}
+    mapping = {
+        "custom.dynamic_mean_window_size": "xpcs.dynamic_mean_window_size",
+        "custom.lld": "xpcs.lld",
+        "custom.sigma": "xpcs.sigma",
+        "custom.snophi": "xpcs.snophi",
+        "custom.snoq": "xpcs.snoq"
+    }
     mdfcc = MDFConnectClient()
-    with open(path_to_json) as f:
+    with open(local_json_path) as f:
         md = json.load(f)
     # DC block (title, authors, publisher, pub year, subjects)
-    mdfcc.create_dc_block(title=os.path.basename(path_to_json).replace(".json", ""),
+    mdfcc.create_dc_block(title=os.path.basename(local_json_path).replace(".json", ""),
                           authors=[creator["creatorName"] for creator in md["creators"]],
                           publisher=md.get("publisher"),
                           publication_year=md.get("publication_year"),
@@ -27,15 +39,17 @@ def mdf_submit():
     # Add JSON mapping
     mdfcc.add_index("json", mapping)
     # Set test flag
-    mdfcc.set_test(test_submission)
-    # Add Argonne as organization
-    mdfcc.add_organization("ANL")
+    mdfcc.set_test(test)
+    # Add XPCS as organization
+    mdfcc.add_organization("XPCS 8-ID")
+    # Set group-by-dir flag
+    mdfcc.set_conversion_config({"group_by_dir": True})
     # Add MDF Publish service
-    if publish_with_doi:
+    if with_doi:
         mdfcc.add_service("mdf_publish")
 
     # Submit dataset
-    sub_res = mdfcc.submit_dataset(update=is_resubmission)
+    sub_res = mdfcc.submit_dataset(update=update)
     if not sub_res["success"]:
         raise RuntimeError(sub_res["error"])
     else:
@@ -43,12 +57,14 @@ def mdf_submit():
         return "Submission '{}' started".format(sub_res["source_id"])
 
 
-def mdf_check_status():
-    """Check status of MDF Connect submission."""
-    from mdf_connect_client import MDFConnectClient
+def mdf_check_status(source_id):
+    """Check status of MDF Connect submission.
 
-    # TODO: Find source_id dynamically
-    source_id = "_test_narayanan_a002_smb_00010256_v1.1"
+    Arguments:
+        source_id (str): The source_id of the dataset to check. The source_id is returned
+                from the submission call.
+    """
+    from mdf_connect_client import MDFConnectClient
 
     mdfcc = MDFConnectClient()
     status = mdfcc.check_status(source_id, raw=True)
