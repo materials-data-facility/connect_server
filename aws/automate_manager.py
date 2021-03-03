d77894ffee59a551032559ac5d742c2b79207980
Ben Galewsky <ben@peartreestudio.net>
2021-03-03 12:29:30 -0600

    Add organizations to flow

--- aws/automate_manager.py
+++ aws/automate_manager.py
@@ -3,6 +3,8 @@ from globus_automate_client import FlowsClient
 
 
 class AutomateManager:
+    petrel_endpoint_id = "e38ee745-6d04-11e5-ba46-22000b92c6ec"
+
     def __init__(self, globus_secrets, scope):
         def cli_authorizer_callback(**kwargs):
             conf_client = globus_sdk.ConfidentialAppAuthClient(
@@ -17,3 +19,38 @@ class AutomateManager:
             authorizer_callback=cli_authorizer_callback,
             authorizer=auth)
         print(self.flows_client)
+
+    def submit(self, mdf_rec):
+        automate_rec = {
+            "mdf_portal_link": "https://example.com/example_link",
+            "user_transfer_inputs": [
+                {
+                    "destination_endpoint_id": self.petrel_endpoint_id,
+                    "label": "MDF Flow Test Transfer1",
+                    "source_endpoint_id": "e38ee745-6d04-11e5-ba46-22000b92c6ec",
+                    "transfer_items": [
+                        {
+                            "destination_path": "/MDF/mdf_connect/test_files/deleteme/data/test123/",
+                            "recursive": True,
+                            "source_path": "/MDF/mdf_connect/test_files/canonical_datasets/dft/"
+                        }
+                    ]
+                }
+            ],
+            "data_destinations": [],
+            "data_permissions": {},
+            "dataset_acl": [
+                "urn:globus:auth:identity:117e8833-68f5-4cb2-afb3-05b25db69be1"
+            ],
+            "search_index": "aeccc263-f083-45f5-ab1d-08ee702b3384",
+            "group_by_dir": True,
+            "mdf_storage_ep": "e38ee745-6d04-11e5-ba46-22000b92c6ec",
+            "mdf_dataset_path": "/MDF/mdf_connect/test_files/deleteme/data/test123/",
+            "dataset_mdata": {},
+            "validator_params": {},
+            "feedstock_https_domain": "https://e38ee745-6d04-11e5-ba46-22000b92c6ec.e.globus.org",
+            "curation_input": False,
+            "mdf_publish": False,
+            "citrine": False,
+            "mrr": False
+        }
---
+++ aws/organization.py
@@ -0,0 +1,47 @@
+import json
+import os
+from collections import namedtuple
+
+import jsonschema
+
+
+class OrganizationException(Exception):
+    pass
+
+
+class OrganizationDatabaseError(jsonschema.ValidationError):
+    pass
+
+
+class Organization:
+    def __init__(self, json_doc):
+        self.json_doc = json_doc
+        pass
+
+    @classmethod
+    def from_schema_repo(cls, canonical_name):
+        schema_path = "../../data-schemas/schemas"
+        with open(os.path.join(schema_path, "..", "connect_aux_data", "organizations.json")) as org_schema:
+            orgs = json.load(org_schema)
+            filtered_orgs = list(filter(lambda org: org['canonical_name'] == canonical_name, orgs))
+            if not filtered_orgs:
+                raise OrganizationException(f"Organization {canonical_name} not found")
+
+            if len(filtered_orgs) > 1:
+                raise OrganizationException("Organization database contains duplicates")
+            o = filtered_orgs[0]
+
+        with open(os.path.join(schema_path, "organization.json")) as schema_file:
+            schema = json.load(schema_file)
+            resolver = jsonschema.RefResolver(base_uri="file://{}/{}/".format(os.getcwd(),
+                                                                              schema_path),
+                                              referrer=schema)
+            try:
+                jsonschema.validate(o, schema, resolver=resolver)
+                object_name = namedtuple("Organization", o.keys())(*o.values())
+
+                return object_name
+
+            except jsonschema.ValidationError as e:
+                print("Error in the organization json document")
+                raise OrganizationDatabaseError(e)
