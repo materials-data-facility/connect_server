cf6b33a1501a34a6f508136e75be8e87fb0b05ee
Ben Galewsky <ben@peartreestudio.net>
2021-04-23 09:50:55 -0500

    Update flow

--- automate/minimus_mdf_flow.py
+++ automate/minimus_mdf_flow.py
@@ -80,10 +80,113 @@ def flow_def(smtp_send_credentials, sender_email, flow_permissions):
                         {
                             "Variable": "$.UserTransferResult.status",
                             "StringEquals": "SUCCEEDED",
-                            "Next": "ChooseNotifyUserEnd"  # Was Xtract
+                            "Next": "ChooseCuration"
                         }
                     ],
                     "Default": "FailUserTransfer"
+                },
+
+                "ChooseCuration": {
+                    "Type": "Choice",
+                    "Choices": [
+                        {
+                            "Variable": "$.curation_input",
+                            "BooleanEquals": False,
+                            "Next": "SearchIngest"
+                        }
+                    ],
+                    "Default": "SendCurationEmail"
+                },
+                "SendCurationEmail": {
+                    "Type": "Action",
+                    "ActionUrl": "https://actions.globus.org/notification/notify",
+                    "ExceptionOnActionFailure": True,
+                    "ResultPath": "$.CurationEmailResult",
+                    "Parameters": {
+                        "body_mimetype": "text/html",
+                        "sender": "materialsdatafacility@uchicago.edu",
+                        "destination": "bengal1@illinois.edu",
+                        "subject": "Materials Data Facility Curation Request",
+                        "body_template": "Please either Approve or Deny the secure egress request here: $landing_page_url",
+                        "body_variables": {
+                            "landing_page_url.=": "'https://actions.globus.org/weboption/landing_page/' + `$._context.action_id`"
+                        },                        "notification_method": "any",
+                        "notification_priority": "high",
+                        "send_credentials": [
+                            {
+                                "credential_method": "email",
+                                "credential_type": "ses",
+                                "credential_value.$": "$._private_email_credentials"
+                            }
+                        ],
+                        "__Private_Parameters": [
+                            "send_credentials"
+                        ]
+                    },
+                    "Next": "CurateSubmission"
+                },
+                "CurateSubmission": {
+                    "Type": "Action",
+                    "ActionUrl": "https://actions.globus.org/weboption/wait_for_option",
+                    "ExceptionOnActionFailure": True,
+                    "ResultPath": "$.CurateResult",
+                    "Parameters": {
+                        "landing_page": {
+                            "url_suffix.$": "$._context.action_id",
+                            "header_background": "#FFF8C6",
+                            "header_icon_url": "https://materialsdatafacility.org/images/MDF-logo@2x.png",
+                            "header_icon_link": "https://materialsdatafacility.org",
+                            "header_text": "Curate an MDF Dataset",
+                            "page_title": "Materials Data Facility",
+                            "preamble_text.=": "'A new dataset has been submitted. ' + `$.mdf_portal_link` +' Please review it to allow processing to continue.'"                        },
+                        "options": [
+                            {
+                                "name": "accepted",
+                                "description": "Accept dataset",
+                                "url_suffix.=": "`$._context.action_id` + '_approve'",
+                                "completed_message": "Indexing of dataset submission will commence"
+                            },
+                            {
+                                "name": "rejected",
+                                "description": "Reject Dataset",
+                                "url_suffix.=": "`$._context.action_id` + '_deny'",
+                                "completed_message": "Submission has been cancelled"
+                            }
+                        ]
+                    },
+                    "WaitTime": 86400,
+                    "Next": "ChooseAcceptance"
+                },
+                "ChooseAcceptance": {
+                    "Type": "Choice",
+                    "Choices": [
+                        {
+                            "Variable": "$.CurateResult.details.name",
+                            "StringEquals": "accepted",
+                            "Next": "SearchIngest"
+                        },
+                        {
+                            "Variable": "$.CurateResult.details.name",
+                            "StringEquals": "rejected",
+                            "Next": "FailCuration"
+                        }
+                    ],
+                    "Default": "ExceptionState"
+                },
+                "SearchIngest":{
+                    "Type": "Action",
+                    "ActionUrl": "https://actions.globus.org/search/ingest",
+                    "ExceptionOnActionFailure": True,
+                    "ResultPath": "$.SearchIngestResult",
+                    "Parameters": {
+                        "search_index.$": "$.search_index",
+                        "subject.$": "$.dataset_mdata.mdf.source_id",
+                        "visible_to": [
+                            "public"
+                        ],
+                        "content.$": "$.dataset_mdata"
+                    },
+                    "Next": "ChooseNotifyUserEnd"
                 },
                 "FailUserTransfer": {
                     "Type": "ExpressionEval",
@@ -101,6 +204,43 @@ def flow_def(smtp_send_credentials, sender_email, flow_permissions):
                         "message.=": "'Your MDF submission ' + `$.source_id` + ' failed to transfer to MDF:\n' + `$.UserPermissionResult.details`"
                     },
                     "ResultPath": "$.FinalState",
+                    "Next": "ChooseNotifyUserEnd"
+                },
+                "FailCuration": {
+                    "Type": "ExpressionEval",
+                    "Parameters": {
+                        "title": "MDF Submission Rejected",
+                        "message.=": "'Your submission (' + `$.source_id` + ') was rejected by a curator and did not complete the ingestion process. The curator gave the following reason for rejection: '+ `$.CurateResult.details.output.CurationResult.details.parameters.user_input`"
+                    },
+                    "ResultPath": "$.FinalState",
+                    "Next": "ChooseNotifyUserEnd"
+                },
+                "ExceptionState": {
+                    "Type": "Action",
+                    "ActionUrl": "https://actions.globus.org/notification/notify",
+                    "ExceptionOnActionFailure": True,
+                    "Parameters": {
+                        "body_mimetype": "text/html",
+                        "sender": "materialsdatafacility@uchicago.edu",
+                        "destination": "bengal1@illinois.edu",
+                        "subject": "Submission Failed to Ingest",
+                        "body_template.=": "'Submission ' + `$.source_id` + ' fatally errored processing in Flow '+ `$._context.action_id` + '. Please review the Flow log for details about this exception.'",
+
+                        "notification_method": "any",
+                        "notification_priority": "high",
+                        "send_credentials": [
+                            {
+                                "credential_method": "email",
+                                "credential_type": "ses",
+                                "credential_value.$": "$._private_email_credentials"
+                            }
+                        ],
+                        "__Private_Parameters": [
+                            "send_credentials"
+                        ]
+                    },
+                    "ResultPath": "$.ExceptionNotifyResult",
+                    "WaitTime": 86400,
                     "Next": "ChooseNotifyUserEnd"
                 },
                 "ChooseNotifyUserEnd": {
