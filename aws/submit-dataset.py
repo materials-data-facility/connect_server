246a1e8cbcd0b3b106673035e9c4a4affcc3dec0
Ben Galewsky <ben@peartreestudio.net>
2021-04-27 10:34:11 -0500

    Clean up config

--- aws/submit-dataset.py
+++ aws/submit-dataset.py
@@ -23,35 +23,15 @@ class ClientException(Exception):
 
 
 CONFIG = {
-    "ADMIN_GROUP_ID": "5fc63928-3752-11e8-9c6f-0e00fd09bf20",
-    "EXTRACT_GROUP_ID": "cc192dca-3751-11e8-90c1-0a7c735d220a",
-    "API_SCOPE": "https://auth.globus.org/scopes/c17f27bb-f200-486a-b785-2a25e82af505/connect",
-    "API_SCOPE_ID": "mdf_dataset_submission",
     "BACKUP_EP": False,
     "BACKUP_PATH": "/mdf_connect/dev/data/",
     "DEFAULT_DOI_TEST": True,
     "DEFAULT_CITRINATION_PUBLIC": False,
     "DEFAULT_MRR_TEST": True,
-    # Regexes for detecting Globus Web App links
-    "GLOBUS_LINK_FORMS": [
-        "^https:\/\/www\.globus\.org\/app\/transfer",
-        # noqa: W605 (invalid escape char '\/')
-        "^https:\/\/app\.globus\.org\/file-manager",  # noqa: W605
-        "^https:\/\/app\.globus\.org\/transfer",  # noqa: W605
-        "^https:\/\/.*globus.*(?=.*origin_id)(?=.*origin_path)",  # noqa: W605
-        "^https:\/\/.*globus.*(?=.*destination_id)(?=.*destination_path)"  # noqa: W605
-    ],
-
-    # Using Prod-P GDrive EP because having two GDrive EPs on one account seems to fail
-    "GDRIVE_EP": "f00dfd6c-edf4-4c8b-a4b1-be6ad92a4fbb",
-    "GDRIVE_ROOT": "/Shared With Me",
 
     "TRANSFER_WEB_APP_LINK": "https://app.globus.org/file-manager?origin_id={}&origin_path={}",
-    "INGEST_URL": "https://dev-api.materialsdatafacility.org/ingest",
     "INGEST_INDEX": "mdf-dev",
     "INGEST_TEST_INDEX": "mdf-dev",
-    "DYNAMO_STATUS_TABLE": "dev-status-alpha-2",
-    "DYNAMO_CURATION_TABLE": "dev-curation-alpha-1"
 }
 
 
@@ -271,7 +251,7 @@ def lambda_handler(event, context):
     metadata["mdf"]["source_id"] = source_id
     metadata["mdf"]["source_name"] = source_name
     metadata["mdf"]["version"] = source_id_info["search_version"]
-    metadata["mdf"]["record_type"] = "dataset"
+
     # Fetch custom block descriptors, cast values to str, turn _description => _desc
     new_custom = {}
     for key, val in metadata.pop("custom", {}).items():
@@ -540,7 +520,6 @@ def lambda_handler(event, context):
         else:
             sub_conf["storage_acl"] = sub_conf["acl"]
 
-    metadata['curation'] = sub_conf['curation']
     status_info = {
         "source_id": source_id,
         "submission_time": datetime.utcnow().isoformat("T") + "Z",
--- aws/submit-status.py
+++ aws/submit-status.py
@@ -1,53 +1,356 @@
+
+# DynamoDB setup
 import json
-from dynamo_manager import DynamoManager
-from automate_manager import AutomateManager
-from utils import get_secret
+import logging
+import os
 
-CONFIG = {
-    "ADMIN_GROUP_ID": "5fc63928-3752-11e8-9c6f-0e00fd09bf20",
-    "EXTRACT_GROUP_ID": "cc192dca-3751-11e8-90c1-0a7c735d220a",
-    "API_SCOPE": "https://auth.globus.org/scopes/c17f27bb-f200-486a-b785-2a25e82af505/connect",
-    "API_SCOPE_ID": "mdf_dataset_submission",
-    "BACKUP_EP": False,
-    "BACKUP_PATH": "/mdf_connect/dev/data/",
-    "DEFAULT_DOI_TEST": True,
-    "DEFAULT_CITRINATION_PUBLIC": False,
-    "DEFAULT_MRR_TEST": True,
-    # Regexes for detecting Globus Web App links
-    "GLOBUS_LINK_FORMS": [
-        "^https:\/\/www\.globus\.org\/app\/transfer",
-        # noqa: W605 (invalid escape char '\/')
-        "^https:\/\/app\.globus\.org\/file-manager",  # noqa: W605
-        "^https:\/\/app\.globus\.org\/transfer",  # noqa: W605
-        "^https:\/\/.*globus.*(?=.*origin_id)(?=.*origin_path)",  # noqa: W605
-        "^https:\/\/.*globus.*(?=.*destination_id)(?=.*destination_path)"  # noqa: W605
-    ],
+import boto3
+import jsonschema
+from boto3.dynamodb.conditions import Attr
+from boto3.dynamodb.conditions import Key
 
-    # Using Prod-P GDrive EP because having two GDrive EPs on one account seems to fail
-    "GDRIVE_EP": "f00dfd6c-edf4-4c8b-a4b1-be6ad92a4fbb",
-    "GDRIVE_ROOT": "/Shared With Me",
+logger = logging.getLogger(__name__)
 
-    "TRANSFER_WEB_APP_LINK": "https://app.globus.org/file-manager?origin_id={}&origin_path={}",
-    "INGEST_URL": "https://dev-api.materialsdatafacility.org/ingest",
-    "INGEST_INDEX": "mdf-dev",
-    "INGEST_TEST_INDEX": "mdf-dev",
-    "DYNAMO_STATUS_TABLE": "dev-status-alpha-2",
-    "DYNAMO_CURATION_TABLE": "dev-curation-alpha-1"
-}
-
-
-def lambda_handler(event, context):
-    dynamo_manager = DynamoManager()
-    automate_manager = AutomateManager(get_secret())
-
-    print(event)
-    source_id = event['pathParameters']['source_id']
-    status_rec = dynamo_manager.for_source_id(source_id)
-    print(status_rec)
-
-    print(automate_manager.get_log(status_rec['action_id']))
-
-    return {
-        'statusCode': 200,
-        'body': json.dumps(automate_manager.get_status(status_rec['action_id']))
+class DynamoManager:
+    DMO_SCHEMA = {
+        # "TableName": DMO_TABLE,
+        "AttributeDefinitions": [{
+            "AttributeName": "source_id",
+            "AttributeType": "S"
+        }],
+        "KeySchema": [{
+            "AttributeName": "source_id",
+            "KeyType": "HASH"
+        }],
+        "ProvisionedThroughput": {
+            "ReadCapacityUnits": 20,
+            "WriteCapacityUnits": 20
+        }
     }
+    STATUS_STEPS = (
+        ("sub_start", "Submission initialization"),
+        ("old_cancel", "Cancellation of previous submissions"),
+        ("data_download", "Connect data download"),
+        ("data_transfer", "Data transfer to primary destination"),
+        ("extracting", "Metadata extraction"),
+        ("curation", "Dataset curation"),
+        ("ingest_search", "MDF Search ingestion"),
+        ("ingest_backup", "Data transfer to secondary destinations"),
+        ("ingest_publish", "MDF Publish publication"),
+        ("ingest_citrine", "Citrine upload"),
+        ("ingest_mrr", "Materials Resource Registration"),
+        ("ingest_cleanup", "Post-processing cleanup")
+    )
+
+    def __init__(self):
+        self.dmo_client = boto3.resource('dynamodb', region_name="us-east-1")
+        self.dmo_tables = {
+            "status": os.environ["DYNAMO_STATUS_TABLE"],
+            "curation": os.environ["DYNAMO_CURATION_TABLE"]
+        }
+
+        # Load status schema
+        schema_path = "./schemas/schemas"
+        with open(os.path.join(schema_path, "internal_status.json")) as schema_file:
+            self.schema = json.load(schema_file)
+
+        self.resolver = jsonschema.RefResolver(base_uri="file://{}/{}/".format(os.getcwd(), schema_path), referrer=self.schema)
+
+    def get_dmo_table(self, table_name):
+        # For compatibility with legacy utils in this file
+        try:
+            table_key = self.dmo_tables[table_name]
+        except KeyError:
+            return {
+                "success": False,
+                "error": "Invalid table '{}'".format(table_name)
+            }
+        try:
+            table = self.dmo_client.Table(table_key)
+            dmo_status = table.table_status
+            if dmo_status != "ACTIVE":
+                raise ValueError("Table not active")
+        except (ValueError, boto3.client.meta.client.exceptions.ResourceNotFoundException):
+            return {
+                "success": False,
+                "error": "Table does not exist or is not active"
+                }
+        except Exception as e:
+            return {
+                "success": False,
+                "error": repr(e)
+                }
+        else:
+            return {
+                "success": True,
+                "table": table
+                }
+
+    def scan_table(self, table_name, fields=None, filters=None):
+        """Scan the status or curation databases..
+
+        Arguments:
+        table_name (str): The Dynamo table to scan.
+        fields (list of str): The fields from the results to return.
+                              Default None, to return all fields.
+        filters (list of tuples): The filters to apply. Format: (field, operator, value)
+                                  For an entry to be returned, all filters must match.
+                                  Default None, to return all entries.
+                               field: The status field to filter on.
+                               operator: The relation of field to value. Valid operators:
+                                         ^: Begins with
+                                         *: Contains
+                                         ==: Equal to (or field does not exist, if value is None)
+                                         !=: Not equal to (or field exists, if value is None)
+                                         >: Greater than
+                                         >=: Greater than or equal to
+                                         <: Less than
+                                         <=: Less than or equal to
+                                         []: Between, inclusive (requires a list of two values)
+                                         in: Is one of the values (requires a list of values)
+                                             This operator effectively allows OR-ing '=='
+                               value: The value of the field.
+
+        Returns:
+        dict: The results of the scan.
+            success (bool): True on success, False otherwise.
+            results (list of dict): The status entries returned.
+            error (str): If success is False, the error that occurred.
+        """
+        # Get Dynamo status table
+        tbl_res = self.get_dmo_table(table_name)
+        print("Table", tbl_res)
+
+        if not tbl_res["success"]:
+            return tbl_res
+        table = tbl_res["table"]
+
+        # Translate fields
+        if isinstance(fields, str) or fields is None:
+            proj_exp = fields
+        elif isinstance(fields, list):
+            proj_exp = ",".join(fields)
+        else:
+            return {
+                "success": False,
+                "error": "Invalid fields type {}: '{}'".format(type(fields), fields)
+            }
+
+        # Translate filters
+        # 0 = field
+        # 1 = operator
+        # 2 = value
+        if isinstance(filters, tuple):
+            filters = [filters]
+        if filters is None or (isinstance(filters, list) and len(filters) == 0):
+            filter_exps = None
+        elif isinstance(filters, list):
+            filter_exps = []
+            for fil in filters:
+                # Begins with
+                if fil[1] == "^":
+                    filter_exps.append(Attr(fil[0]).begins_with(fil[2]))
+                # Contains
+                elif fil[1] == "*":
+                    filter_exps.append(Attr(fil[0]).contains(fil[2]))
+                # Equal to (or field does not exist, if value is None)
+                elif fil[1] == "==":
+                    if fil[2] is None:
+                        filter_exps.append(Attr(fil[0]).not_exists())
+                    else:
+                        filter_exps.append(Attr(fil[0]).eq(fil[2]))
+                # Not equal to (or field exists, if value is None)
+                elif fil[1] == "!=":
+                    if fil[2] is None:
+                        filter_exps.append(Attr(fil[0]).exists())
+                    else:
+                        filter_exps.append(Attr(fil[0]).ne(fil[2]))
+                # Greater than
+                elif fil[1] == ">":
+                    filter_exps.append(Attr(fil[0]).gt(fil[2]))
+                # Greater than or equal to
+                elif fil[1] == ">=":
+                    filter_exps.append(Attr(fil[0]).gte(fil[2]))
+                # Less than
+                elif fil[1] == "<":
+                    filter_exps.append(Attr(fil[0]).lt(fil[2]))
+                # Less than or equal to
+                elif fil[1] == "<=":
+                    filter_exps.append(Attr(fil[0]).lte(fil[2]))
+                # Between, inclusive (requires a list of two values)
+                elif fil[1] == "[]":
+                    if not isinstance(fil[2], list) or len(fil[2]) != 2:
+                        return {
+                            "success": False,
+                            "error": "Invalid between ('[]') operator values: '{}'".format(
+                                fil[2])
+                        }
+                    filter_exps.append(Attr(fil[0]).between(fil[2][0], fil[2][1]))
+                # Is one of the values (requires a list of values)
+                elif fil[1] == "in":
+                    if not isinstance(fil[2], list):
+                        return {
+                            "success": False,
+                            "error": "Invalid 'in' operator values: '{}'".format(fil[2])
+                        }
+                    filter_exps.append(Attr(fil[0]).is_in(fil[2]))
+                else:
+                    return {
+                        "success": False,
+                        "error": "Invalid filter operator '{}'".format(fil[1])
+                    }
+        else:
+            return {
+                "success": False,
+                "error": "Invalid filters type {}: '{}'".format(type(filters), filters)
+            }
+
+        # Make scan arguments
+        scan_args = {
+            "ConsistentRead": True
+        }
+        if proj_exp is not None:
+            scan_args["ProjectionExpression"] = proj_exp
+        if filter_exps is not None:
+            # Create valid FilterExpression
+            # Each Attr must be combined with &
+            filter_expression = filter_exps[0]
+            for i in range(1, len(filter_exps)):
+                filter_expression = filter_expression & filter_exps[i]
+            scan_args["FilterExpression"] = filter_expression
+
+        # Make scan call, paging through if too many entries are scanned
+        result_entries = []
+        print("Scan ", scan_args)
+        while True:
+            scan_res = table.scan(**scan_args)
+            # Check for success
+            if scan_res["ResponseMetadata"]["HTTPStatusCode"] >= 300:
+                return {
+                    "success": False,
+                    "error": ("HTTP code {} returned: {}"
+                              .format(scan_res["ResponseMetadata"]["HTTPStatusCode"],
+                                      scan_res["ResponseMetadata"]))
+                }
+            # Add results to list
+            result_entries.extend(scan_res["Items"])
+            # Check for completeness
+            # If LastEvaluatedKey exists, need to page through more results
+            if scan_res.get("LastEvaluatedKey", None) is not None:
+                scan_args["ExclusiveStartKey"] = scan_res["LastEvaluatedKey"]
+            # Otherwise, all results retrieved
+            else:
+                break
+
+        return {
+            "success": True,
+            "results": result_entries
+        }
+
+    def validate_status(self, status, new_status=False):
+        """Validate a submission status.
+
+        Arguments:
+        status (dict): The status to validate.
+        new_status (bool): Is this status a new status?
+
+        Returns:
+        dict:
+            success: True if the status is valid, False if not.
+            error: If the status is not valid, the reason why. Only present when success is False.
+            details: Optional further details about an error.
+        """
+        # Validate against status schema
+        try:
+            jsonschema.validate(status, self.schema, resolver=self.resolver)
+        except jsonschema.ValidationError as e:
+            return {
+                "success": False,
+                "error": "Invalid status: {}".format(str(e).split("\n")[0]),
+                "details": str(e)
+            }
+
+        code = status["code"]
+        try:
+            assert len(code) == len(self.STATUS_STEPS)
+            if new_status:
+                # Nothing started or finished
+                assert code == "z" * len(code)
+        except AssertionError:
+            return {
+                "success": False,
+                "error": ("Invalid status code '{}' for {} status"
+                          .format(code, "new" if new_status else "old"))
+            }
+        else:
+            return {
+                "success": True
+            }
+
+    def old_read_table(self, table_name, source_id):
+        # Compatibility for legacy utils in this file
+        tbl_res = self.get_dmo_table(table_name)
+        if not tbl_res["success"]:
+            return tbl_res
+        table = tbl_res["table"]
+
+        entry = table.get_item(Key={"source_id": source_id}, ConsistentRead=True).get(
+            "Item")
+        if not entry:
+            return {
+                "success": False,
+                "error": "ID {} not found in {} database".format(source_id, table_name)
+            }
+        return {
+            "success": True,
+            "status": entry
+        }
+
+    def create_status(self, status):
+        tbl_res = self.get_dmo_table("status")
+        if not tbl_res["success"]:
+            return tbl_res
+        table = tbl_res["table"]
+
+        # Add defaults
+        status["messages"] = ["No message available"] * len(self.STATUS_STEPS)
+        status["active"] = True
+        status["cancelled"] = False
+        status["pid"] = os.getpid()
+        status["extensions"] = []
+        status["hibernating"] = False
+        status["code"] = "z" * len(self.STATUS_STEPS)
+        status["updates"] = []
+
+        status_valid = self.validate_status(status, new_status=True)
+        if not status_valid["success"]:
+            return status_valid
+
+        # Check that status does not already exist
+        if self.old_read_table("status", status["source_id"])["success"]:
+            return {
+                "success": False,
+                "error": "ID {} already exists in status database".format(status["source_id"])
+            }
+        try:
+            table.put_item(Item=status, ConditionExpression=Attr("source_id").not_exists())
+        except Exception as e:
+            return {
+                "success": False,
+                "error": repr(e)
+            }
+        else:
+            logger.info("Status for {}: Created".format(status["source_id"]))
+            return {
+                "success": True,
+                "status": status
+            }
+
+    def for_source_id(self, source_id):
+        table = self.get_dmo_table("status")
+        response = table['table'].query(
+            KeyConditionExpression=Key('source_id').eq(source_id))
+        assert "Items" in response
+        assert len(response['Items']) == 1
+
+        return response['Items'][0]
