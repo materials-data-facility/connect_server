5d0ff01f894dcdc91d27cd2d7e93389c9e4bf904
Ben Galewsky <ben@peartreestudio.net>
2021-09-28 10:58:05 -0500

    Add script to retreive MDF Bearer token

---
+++ scripts/get_mdf_token.py
@@ -0,0 +1,7 @@
+from mdf_connect_client import MDFConnectClient
+"""
+This script authenticates the user against the MDF Application and prints out the 
+bearer token that can be used in REST calls
+"""
+mdfcc = MDFConnectClient(service_instance="dev")
+print(mdfcc._MDFConnectClient__authorizer.access_token)
