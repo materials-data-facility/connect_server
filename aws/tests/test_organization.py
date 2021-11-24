10ceadfbfa95fc5fa06dff700ffe0fe80681ba10
Ben Galewsky <ben@peartreestudio.net>
2021-11-23 20:31:48 -0600

    Update organization test to pick up org that survived the cull

--- aws/tests/test_organization.py
+++ aws/tests/test_organization.py
@@ -4,11 +4,11 @@ from organization import Organization
 class TestOrganization:
     def test_from_schema_repo(self):
         # Find by canonical name
-        nrel = Organization.from_schema_repo("National Renewable Energy Laboratory")
-        assert nrel
-        assert nrel.canonical_name == "National Renewable Energy Laboratory"
+        midas = Organization.from_schema_repo("AFRL Additive Manufacturing Challenge")
+        assert midas
+        assert midas.canonical_name == "AFRL Additive Manufacturing Challenge"
 
         # Find by Alias
-        nrel2 = Organization.from_schema_repo("NREL")
-        assert nrel2
-        assert nrel2.canonical_name == "National Renewable Energy Laboratory"
+        midas2 = Organization.from_schema_repo("MIDAS")
+        assert midas2
+        assert midas2.canonical_name == "AFRL Additive Manufacturing Challenge"
