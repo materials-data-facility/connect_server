763e1dc9c6efe07dd06090fbdb234e17603f035e
Ben Galewsky <ben@peartreestudio.net>
2023-02-21 08:45:05 -0600

    Cleanup unused files

--- mdf_connect_server/processor/__init__.py
+++
@@ -1,6 +0,0 @@
-# flake8: noqa
-# NOTE: flake8 complains about these imports going unused; this is fine
-from .extractors import run_extractors
-from .validator import Validator
-from .start_extractors import start_extractors
-from .processor import processor
