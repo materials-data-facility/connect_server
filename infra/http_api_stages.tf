4d3d828ee8f1144a716dfd5dea73af1047fc4c57
Eric Blau <blau@anl.gov>
2023-08-14 09:31:25 -0500

    WIP

--- infra/http_api_stages.tf
+++ infra/http_api_stages.tf
@@ -4,6 +4,15 @@ resource "aws_apigatewayv2_stage" "prod" {
   name        = "prod"
   api_id      = aws_apigatewayv2_api.http_api.id
   auto_deploy = true
+  stage_variables = {
+    auth_function = "MDF-Connect-auth-prod"
+    submit_function = "MDF-Connect-submit-prod"
+    status_function = "MDF-Connect-status-prod"
+  }
+  access_log_settings  {
+    destination_arn = "${aws_cloudwatch_log_group.MDFConnect-logs.arn}"
+    format =  "$context.identity.sourceIp $context.identity.caller $context.identity.user [$context.requestTime] $context.httpMethod $context.resourcePath $context.protocol $context.status $context.responseLength $context.requestId $context.extendedRequestId $context.authorizer.error $context.authorizer.principalId $context.authorizer.claims"
+  }
 }
 
 # Define the AWS API Gateway v2 deployment for testing
@@ -11,6 +20,15 @@ resource "aws_apigatewayv2_stage" "test" {
   name        = "test"
   api_id      = aws_apigatewayv2_api.http_api.id
   auto_deploy = true
+  stage_variables = {
+    auth_function = "MDF-Connect-auth-test"
+    submit_function = "MDF-Connect-submit-test"
+    status_function = "MDF-Connect-status-test"
+  }
+  access_log_settings  {
+    destination_arn = "${aws_cloudwatch_log_group.MDFConnect-logs.arn}"
+    format =  "$context.extendedRequestId $context.identity.sourceIp $context.identity.caller $context.identity.user [$context.requestTime] $context.httpMethod $context.resourcePath $context.protocol $context.status $context.responseLength $context.requestId $context.extendedRequestId"
+  }
 }# Output the URLs for each environment
 output "auth_url" {
   value = "${aws_apigatewayv2_stage.prod.invoke_url}/auth"
