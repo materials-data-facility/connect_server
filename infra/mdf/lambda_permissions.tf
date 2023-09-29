4f736319c24b14120398efe502372c33884b079a
Eric Blau <blau@anl.gov>
2023-09-29 11:14:56 -0500

    fix lambda permissions so that gateway can trigger them.

--- infra/mdf/lambda_permissions.tf
+++ infra/mdf/lambda_permissions.tf
@@ -1,23 +1,26 @@
 resource "aws_lambda_permission" "lambda_auth_permission" {
+  for_each = local.environments
   statement_id  = "AllowAPIGatewayInfoke"
   action        = "lambda:InvokeFunction"
-  function_name = "MDF-Connect-auth-prod"
-  principal     = "apigateway.amazonaws.com"
-
-  # The /* part allows invocation from any stage, method and resource path
-  # within API Gateway.
-  #source_arn = "${aws_api_gateway_rest_api.MyDemoAPI.execution_arn}/*"
-  #source_arn = "${aws_apigatewayv2_api.http_api.execution_arn}/*"
-}
-resource "aws_lambda_permission" "lambda_submit_permission" {
-  statement_id  = "AllowAPIGatewayInfoke"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.mdf-connect-containerized-submit["prod"].function_name
+  function_name = aws_lambda_function.mdf-connect-containerized-auth["${each.key}"].function_name
   principal     = "apigateway.amazonaws.com"
   source_arn    = "${aws_apigatewayv2_api.http_api.execution_arn}/*/*"
+}
 
-  # The /* part allows invocation from any stage, method and resource path
-  # within API Gateway.
-  #source_arn = "${aws_api_gateway_rest_api.MyDemoAPI.execution_arn}/*"
-  #source_arn = "${aws_apigatewayv2_api.http_api.execution_arn}/*"
+resource "aws_lambda_permission" "lambda_submit_permission" {
+  for_each = local.environments
+  statement_id  = "AllowAPIGatewayInfoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.mdf-connect-containerized-submit["${each.key}"].function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.http_api.execution_arn}/*/*"
+}
+
+resource "aws_lambda_permission" "lambda_status_permission" {
+  for_each = local.environments
+  statement_id  = "AllowAPIGatewayInfoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.mdf-connect-containerized-status["${each.key}"].function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.http_api.execution_arn}/*/*"
 }
