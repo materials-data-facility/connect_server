c5aa34ed801b31fe563daf9852e46529c5e4e14a
Ben Galewsky <ben@peartreestudio.net>
2023-10-04 15:37:54 -0500

    Avoid error when updating the Dynamo DB table

--- infra/mdf/dynamodb.tf
+++ infra/mdf/dynamodb.tf
@@ -16,8 +16,9 @@ resource "aws_dynamodb_table" "dynamodb-table" {
     type = "S"
   }
 
+  # Workaround frm https://github.com/hashicorp/terraform-provider-aws/issues/10304#issuecomment-1672617928
   ttl {
-    attribute_name = "TimeToExist"
+    attribute_name = ""
     enabled        = false
   }
 
