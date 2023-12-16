3df1595c3537f6f998642f995fa7c1ddd9100b6c
Ben Galewsky <ben@peartreestudio.net>
2023-12-16 09:00:20 -0600

    Parameterize get_mdf_token

--- scripts/get_mdf_token.py
+++ scripts/get_mdf_token.py
@@ -1,7 +1,13 @@
+import sys
+
 from mdf_connect_client import MDFConnectClient
 """
 This script authenticates the user against the MDF Application and prints out the 
 bearer token that can be used in REST calls
 """
-mdfcc = MDFConnectClient(service_instance="dev")
+if len(sys.argv) != 2:
+    print("Usage: python get_mdf_token.py <MDF Application URL>")
+    sys.exit(1)
+
+mdfcc = MDFConnectClient(service_instance=sys.argv[1])
 print(mdfcc._MDFConnectClient__authorizer.access_token)
