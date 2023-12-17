244591cfc0807401b06467c24c3964f8fa11ac45
Ben Galewsky <ben@peartreestudio.net>
2023-12-17 15:57:14 -0600

    Update runas scope and allow deployment of flow with prod client id

--- infra/mdf/prod/variables.tf
+++ infra/mdf/prod/variables.tf
@@ -30,7 +30,7 @@ variable "env_vars" {
         MANAGE_FLOWS_SCOPE="https://auth.globus.org/scopes/eec9b274-0c81-4334-bdc2-54e90e689b9a/manage_flows"
         MONITOR_BY_GROUP="urn:globus:groups:id:5fc63928-3752-11e8-9c6f-0e00fd09bf20"
         PORTAL_URL="https://acdc.alcf.anl.gov/mdf/detail/"
-        RUN_AS_SCOPE="0c7ee169-cefc-4a23-81e1-dc323307c863"
+        RUN_AS_SCOPE="8ca9aa59-fe92-4525-b607-996eec8d5043"
         SEARCH_INDEX_UUID="1a57bbe5-5272-477f-9d31-343b8258b7a5"
         TEST_DATA_DESTINATION="globus://f10a69a9-338c-4e5b-baa1-0dc92359ab47/mdf_testing/"
         TEST_SEARCH_INDEX_UUID="5acded0c-a534-45af-84be-dcf042e36412"
